3508446a83f101bec033175b429e9bc091a895aa
vagisha312 <108082732+vagisha312@users.noreply.github.com>
2026-05-17 23:51:14 +0530

    Time: 3 ms (29.21%) | Memory: 20.7 MB (92.82%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,35 +1,68 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<int>r;
-        set<int>c;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++)
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int fr = 0;
+        int fc = 0;
+
+        // Check first column
+        for(int i = 0; i < n; i++)
         {
-            for(int j=0;j<m;j++)
+            if(matrix[i][0] == 0)
+                fc = 1;
+        }
+
+        // Check first row
+        for(int j = 0; j < m; j++)
+        {
+            if(matrix[0][j] == 0)
+                fr = 1;
+        }
+
+        // Mark rows and columns
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
             {
-                if(matrix[i][j]==0)
+                if(matrix[i][j] == 0)
                 {
-                    r.insert(i);
-                    c.insert(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=0;i<n;i++)
+
+        // Set inner matrix zeroes
+        for(int i = 1; i < n; i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j = 1; j < m; j++)
             {
-                if(r.find(i)!=r.end())
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                 {
-                    matrix[i][j]=0;
-                }
-                if(c.find(j)!=c.end())
-                {
-                    matrix[i][j]=0;
+                    matrix[i][j] = 0;
                 }
             }
         }
-        
+
+        // Set first row
+        if(fr)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set first column
+        if(fc)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
